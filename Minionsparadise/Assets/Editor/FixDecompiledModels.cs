710613d261cf14c8f01575b2dc52e94dab1de45c
Wednesday Addams <21669120+wednesday2024@users.noreply.github.com>
2026-04-18 14:49:40 -0500

    Fixes.

--- Minionsparadise/Assets/Editor/FixDecompiledModels.cs
+++ Minionsparadise/Assets/Editor/FixDecompiledModels.cs
@@ -1,67 +1,23 @@
 using UnityEngine;
 using UnityEditor;
-using System.Collections.Generic;
 
-public class FixDecompiledModels
+public class MeshRepairPipeline
 {
-    private const string TARGET_PATH = "Assets/";
+    private const string TARGET_PATH = "Assets";
 
-    private static readonly string[] SKIP_FOLDERS = new string[]
-    {
-        "Modules",
-        "Town",
-        "Boardwalk",
-        "Diving",
-        "BoxDimension",
-        "MtBlizzard",
-        "Beach",
-        "HerbertBase",
-        "MtBlizzardSummit"
-    };
-
-    [MenuItem("Project/Models/Fix Decompiled Models")]
+    [MenuItem("Project/Models/Fix Decompiled Meshes")]
     public static void FixMeshes()
     {
         string[] guids = AssetDatabase.FindAssets("t:Mesh", new[] { TARGET_PATH });
 
-        int total = guids.Length;
-
-        for (int index = 0; index < total; index++)
+        for (int i = 0; i < guids.Length; i++)
         {
-            string guid = guids[index];
-            string path = AssetDatabase.GUIDToAssetPath(guid);
+            string path = AssetDatabase.GUIDToAssetPath(guids[i]);
 
-            if (EditorUtility.DisplayCancelableProgressBar("Fixing Models", path, (float)index / Mathf.Max(1, total)))
+            if (EditorUtility.DisplayCancelableProgressBar("Fixing Models", path, (float)i / guids.Length))
                 break;
-
-            if (!path.EndsWith(".asset"))
-                continue;
-
-            if (ShouldSkip(path))
-                continue;
 
             Mesh mesh = AssetDatabase.LoadAssetAtPath<Mesh>(path);
-            if (mesh == null)
-                continue;
-
-            FixMesh(mesh);
-        }
-
-        SkinnedMeshRenderer[] renderers = Object.FindObjectsByType<SkinnedMeshRenderer>(FindObjectsSortMode.None);
-
-        int rTotal = renderers.Length;
-
-        for (int i = 0; i < rTotal; i++)
-        {
-            SkinnedMeshRenderer r = renderers[i];
-
-            if (EditorUtility.DisplayCancelableProgressBar("Fixing Models", r.name, (float)i / Mathf.Max(1, rTotal)))
-                break;
-
-            if (r == null)
-                continue;
-
-            Mesh mesh = r.sharedMesh;
             if (mesh == null)
                 continue;
 
@@ -75,107 +31,56 @@ public class FixDecompiledModels
 
     private static void FixMesh(Mesh mesh)
     {
-        Vector3[] vertices = mesh.vertices;
-        if (vertices == null || vertices.Length == 0)
+        int vCount = mesh.vertexCount;
+        if (vCount == 0)
             return;
 
-        Vector2[] uv0Source = mesh.uv;
-        Vector2[] uv1Source = mesh.uv2;
+        Vector3[] vertices = mesh.vertices;
 
-        Color32[] colorsSource = mesh.colors32;
-        Color32[] colors;
+        Vector2[] uv0 = mesh.uv;
+        if (uv0 == null || uv0.Length != vCount)
+            uv0 = new Vector2[vCount];
 
-        if (colorsSource != null && colorsSource.Length == vertices.Length)
+        Vector2[] uv1 = mesh.uv2;
+        if (uv1 == null || uv1.Length != vCount)
+            uv1 = new Vector2[vCount];
+
+        Color32[] colors = mesh.colors32;
+        if (colors == null || colors.Length != vCount || IsAllZero(colors))
         {
-            colors = new Color32[vertices.Length];
-            for (int i = 0; i < vertices.Length; i++)
-                colors[i] = colorsSource[i];
-        }
-        else if (colorsSource != null && colorsSource.Length > 0)
-        {
-            colors = colorsSource;
-        }
-        else
-        {
-            colors = new Color32[vertices.Length];
-            for (int i = 0; i < vertices.Length; i++)
+            colors = new Color32[vCount];
+            for (int i = 0; i < vCount; i++)
                 colors[i] = new Color32(255, 255, 255, 255);
         }
 
-        Mesh newMesh = new Mesh();
-        newMesh.name = mesh.name;
-
-        newMesh.indexFormat = vertices.Length > 65535
-            ? UnityEngine.Rendering.IndexFormat.UInt32
-            : UnityEngine.Rendering.IndexFormat.UInt16;
-
-        newMesh.SetVertices(vertices);
-
-        if (mesh.normals != null && mesh.normals.Length == vertices.Length)
-            newMesh.SetNormals(mesh.normals);
-        else
-            newMesh.RecalculateNormals();
-
-        if (mesh.tangents != null && mesh.tangents.Length == vertices.Length)
-            newMesh.SetTangents(mesh.tangents);
-        else
-            newMesh.RecalculateTangents();
-
-        if (uv0Source != null && uv0Source.Length == vertices.Length)
+        Vector3[] normals = mesh.normals;
+        if (normals == null || normals.Length != vCount)
         {
-            newMesh.SetUVs(0, new List<Vector2>(uv0Source));
-        }
-        else
-        {
-            Vector2[] uv0 = new Vector2[vertices.Length];
-            for (int i = 0; i < uv0.Length; i++)
-                uv0[i] = Vector2.zero;
-
-            newMesh.SetUVs(0, new List<Vector2>(uv0));
+            mesh.RecalculateNormals();
+            normals = mesh.normals;
         }
 
-        if (uv1Source != null && uv1Source.Length == vertices.Length)
-        {
-            newMesh.SetUVs(1, new List<Vector2>(uv1Source));
-        }
-        else
-        {
-            Vector2[] uv1 = new Vector2[vertices.Length];
-            for (int i = 0; i < uv1.Length; i++)
-                uv1[i] = Vector2.zero;
+        Vector4[] tangents = mesh.tangents;
+        if ((tangents == null || tangents.Length != vCount) && uv0 != null && normals != null)
+            mesh.RecalculateTangents();
 
-            newMesh.SetUVs(1, new List<Vector2>(uv1));
-        }
+        mesh.RecalculateBounds();
 
-        newMesh.SetColors(colors);
-
-        newMesh.subMeshCount = mesh.subMeshCount;
-
-        for (int i = 0; i < mesh.subMeshCount; i++)
-        {
-            newMesh.SetIndices(mesh.GetIndices(i), mesh.GetTopology(i), i);
-        }
-
-        newMesh.bindposes = mesh.bindposes;
-        newMesh.boneWeights = mesh.boneWeights;
-
-        newMesh.RecalculateBounds();
-
-        EditorUtility.CopySerialized(newMesh, mesh);
-
-        mesh.UploadMeshData(false);
+        mesh.uv = uv0;
+        mesh.uv2 = uv1;
+        mesh.colors32 = colors;
 
         EditorUtility.SetDirty(mesh);
         AssetDatabase.SaveAssetIfDirty(mesh);
     }
 
-    private static bool ShouldSkip(string path)
+    private static bool IsAllZero(Color32[] colors)
     {
-        for (int i = 0; i < SKIP_FOLDERS.Length; i++)
+        for (int i = 0; i < colors.Length; i++)
         {
-            if (path.Contains("/" + SKIP_FOLDERS[i] + "/"))
-                return true;
+            if (colors[i].r != 0 || colors[i].g != 0 || colors[i].b != 0 || colors[i].a != 0)
+                return false;
         }
-        return false;
+        return true;
     }
 }
